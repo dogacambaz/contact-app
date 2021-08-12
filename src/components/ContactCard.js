6f7076e777bc74df8cb9eb00a674312b79d8a3fb
Doga Cambaz <D.Cambaz@student.tudelft.nl>
2021-08-12 14:34:14 +0300

    the working app including Contact list and cards, CRUD operations, using axios and JSON server

---
+++ src/components/ContactCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../images/user.jpg";
+
+const CardContact = (props) => {
+  const { id, name, email } = props.contact;
+  return (
+    <div className="item">
+      <img className="ui avatar image" src={user} alt="user" />
+      <div className="content">
+        <Link
+          to={{
+            pathname: "/contacts/" + id,
+            state: { contact: props.contact },
+          }}
+        >
+          <div className="header">{name}</div>
+          <div>{email}</div>
+        </Link>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px", marginLeft: "10 px" }}
+        onClick={() => props.clickHandler(id)}
+      ></i>
+      <Link
+        to={{
+          pathname: "/edit/" + id,
+          state: { contact: props.contact },
+        }}
+      >
+        <i
+          className="edit alternate outline icon"
+          style={{ color: "blue", marginTop: "7px" }}
+        ></i>
+      </Link>
+    </div>
+  );
+};
+
+export default CardContact;
